f2e8940d5e0a7ca88cc3fa63dea5744974676f42
frranq0 <91351705+frranq0@users.noreply.github.com>
2024-04-10 01:49:22 +0000

    devices.txt

---
+++ evaluacion1.py
@@ -0,0 +1,1 @@
+print ("Evaluacion nº1 CCNA DEVNET""\nCreado por Franko Bastias")
---
+++ evaluacion1ej2.py
@@ -0,0 +1,16 @@
+print("********************")
+print("EJERCICIO Nº2")
+print("********************")
+nombre=input("¿Cual es su nombre?: ")
+print("********************")
+apellido=input("¿Cual es su apellido?: ")
+print("********************")
+seccion=input("¿Cual es su seccion?: ")
+print("********************")
+sede=input("¿Cual es su sede?: ")
+print("********************")
+
+
+space=" "
+print("Su nombre y apellido es: ",nombre+space+apellido)
+print("Ademas, su seccion y su sede son: ",seccion+space+sede)
